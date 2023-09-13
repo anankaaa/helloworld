ad88bf6d30966bb75f4767b34da6ee540a0f24a5
Anankaaa <hegyaljai83@hotmail.com>
2023-09-13 18:20:12 +0200

    constructor-solution

--- src/main/java/solutionConstructor/StoreMain.java
+++ src/main/java/solutionConstructor/StoreMain.java
@@ -18,3 +18,24 @@ public class StoreMain {
         System.out.println(paperStore.getStock());
     }
 }
+
+/* Gyakorlati feladat - Raktárkészlet
+Készíts egy Store osztályt, mely egy raktárt modellez.
+A raktár jellemzője az, hogy miből (product) és
+aktuálisan mekkora mennyiséget (stock) tárol.
+(Ennek a raktárnak speciális jellemzője, hogy csak egyféle terméket tud tárolni.)
+Az első attribútuma String, a második int típusú.
+
+A Store példányosításakor elég megadni a tárolt terméket,
+a mennyiség mindig 0, ezért a konstruktor csak a terméket kapja meg kívülről.
+
+Készíts hozzá két metódust, mely a tárolt mennyiséget változtatja:
+a store() metódusa a paraméterként kapott mennyiséget eltárolja a
+raktárban növelve ezzel a készletet, míg a dispatch() metódusa
+az elszállítást modellezi, azaz a paraméterként kapott mennyiséggel
+csökkenti a készletet! (Most még nem kell ellenőrizned,
+hogy elszállításkor van-e a raktárban elegendő mennyiségű terméked.)
+
+A StoreMain osztály main() metódusában készíts két Store példányt,
+és teszteld, hogy mindkét raktár helyesen és függetlenül működik-e
+be- és kiszállítás esetén is! */
